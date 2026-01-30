adcf4373bfe0c4002ace3e01085bf58ba2296b2c
silee <miira6010@gmail.com>
2026-01-30 19:50:27 +0900

    260130 silee - 테스트커밋

--- Monthly_Excel/CrawlingProcessor.cs
+++ Monthly_Excel/CrawlingProcessor.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using ClosedXML.Excel;
-using Monthly_Excel; // KeywordProcessor.ProcessKeywords 사용 가정
+using Monthly_Excel;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
